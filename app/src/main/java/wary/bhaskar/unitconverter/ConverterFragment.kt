fad81827bf5322699b9c1d3a8b261fb3db1a111d
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-06-28 11:49:09 +0530

    Chnages in UI

--- app/src/main/java/wary/bhaskar/unitconverter/ConverterFragment.kt
+++ app/src/main/java/wary/bhaskar/unitconverter/ConverterFragment.kt
@@ -1,59 +1,91 @@
 package wary.bhaskar.unitconverter
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.annotation.AnimRes
+import androidx.fragment.app.viewModels
+import wary.bhaskar.unitconverter.databinding.FragmentConverterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ConverterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConverterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private val converterViewModel: ConverterViewModel by viewModels()
+    private var _binding: FragmentConverterBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_converter, container, false)
+        _binding = FragmentConverterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.iconReplaceUnitTypes.setOnClickListener {
+            animateView(requireContext(), binding.iconReplaceUnitTypes, R.anim.icon_rotation_anim)
+            converterViewModel.changeState()
+            animateView(requireContext(), binding.cardOne, R.anim.fade_in)
+            animateView(requireContext(), binding.cardTwo, R.anim.fade_in)
+        }
+
         /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConverterFragment.
+         * Swapping unit type textViews of two cards
          */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConverterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        converterViewModel.millilitresToOuncesState.observe(viewLifecycleOwner) {
+            if (converterViewModel.millilitresToOuncesState.value == true) {
+                binding.unitTypeTextviewCardOne.text = getString(R.string.millilitres)
+                binding.unitTypeTextviewCardTwo.text = getString(R.string.ounces)
+            } else {
+                binding.unitTypeTextviewCardOne.text = getString(R.string.ounces)
+                binding.unitTypeTextviewCardTwo.text = getString(R.string.millilitres)
             }
+            binding.finalResultTextView.text = null
+        }
+
+        /**
+         * Displaying the final converted result
+         */
+        converterViewModel.finalResult.observe(viewLifecycleOwner) { newFinalResult ->
+            binding.finalResultTextView.text = getString(
+                R.string.final_result,
+                newFinalResult
+            )
+        }
+
+        /**
+         * In case, invalid amount input occurs
+         */
+        converterViewModel.isAmountInvalid.observe(viewLifecycleOwner) {
+            if (converterViewModel.isAmountInvalid.value == true) {
+                binding.inputAmountTextField.isErrorEnabled = true
+                binding.inputAmountEditText.error = "Invalid amount"
+            } else binding.inputAmountTextField.isCounterEnabled = false
+        }
+
+        /**
+         * converting the input amount when tapped
+         */
+        binding.calculateButton.setOnClickListener { convertInputAmount() }
+    }
+
+    private fun animateView(context: Context, view: View, @AnimRes animationId: Int) {
+        val animation = AnimationUtils.loadAnimation(context, animationId)
+        view.startAnimation(animation)
+    }
+
+    private fun convertInputAmount() {
+        val inputAmount = binding.inputAmountEditText.text.toString()
+        if (binding.roundUpSwitch.isChecked) {
+            converterViewModel.calculate(inputAmount.toDoubleOrNull(), true)
+        } else converterViewModel.calculate(inputAmount.toDoubleOrNull(), false)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/wary/bhaskar/unitconverter/ConverterViewModel.kt
+++ app/src/main/java/wary/bhaskar/unitconverter/ConverterViewModel.kt
@@ -1,4 +1,60 @@
 package wary.bhaskar.unitconverter
 
-class ConverterViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.math.RoundingMode
+
+class ConverterViewModel : ViewModel() {
+
+    //state of conversion
+    private var _millilitresToOuncesState = MutableLiveData(true)
+    val millilitresToOuncesState: LiveData<Boolean> get() = _millilitresToOuncesState
+
+    //when there is an invalid amount
+    private var _isAmountInvalid = MutableLiveData<Boolean>()
+    val isAmountInvalid: LiveData<Boolean> get() = _isAmountInvalid
+
+    //final result
+    private val _finalResult = MutableLiveData<String>()
+    val finalResult: LiveData<String> get() = _finalResult
+
+    fun changeState() {
+        _millilitresToOuncesState.value = !_millilitresToOuncesState.value!!
+    }
+
+    private fun formatLongDecimal(
+        decimalNumber: Double,
+        decimalPlaces: Int
+    ): String = decimalNumber
+        .toBigDecimal()
+        .setScale(decimalPlaces, RoundingMode.HALF_EVEN)
+        .toString()
+
+    fun calculate(amount: Double?, isRounded: Boolean) {
+        /**
+         *Checking input amount validity
+         */
+        if (amount == null || amount == 0.0) {
+            _isAmountInvalid.value = true
+        } else {
+            _isAmountInvalid.value = false
+
+            /**
+             * Checking the conversion state
+             */
+            if (_millilitresToOuncesState.value == true) {
+                val result: Double = amount / 29.574
+                _finalResult.value = if (isRounded) {
+                    formatLongDecimal(result, 1)
+                } else formatLongDecimal(result, 3)
+            } else {
+                val result: Double = amount * 29.5735
+                _finalResult.value = if (isRounded) {
+                    formatLongDecimal(result, 1)
+                } else formatLongDecimal(result, 3)
+
+            }
+        }
+    }
 }
